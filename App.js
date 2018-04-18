20aeff5039a90e5238e0b4585586549c3c1703f7
JoungMan Son <sonjm1758@gmail.com>
2018-04-18 18:32:50 +0900

    completed

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, StatusBar, PermissionsAndroid } from 'react-native';
 import Weather from './Weather';
 
+const WEATHER_API_KEY = '820c8c4e1db78d371de3fca298fcc394';
+
 export default class App extends Component {
   state = {
     isLoaded : false,
@@ -13,10 +15,12 @@ export default class App extends Component {
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       (position)=>{
-        this.setState({
-          isLoaded:true,
-          location:position,
-        })
+        console.log(position);
+        this._fetchWeather(position.coords.latitude,position.coords.longitude)
+        // this.setState({
+        //   isLoaded:true,
+        //   location:position,
+        // })
       },
       (error) => {
         this.setState({ error: error });
@@ -24,11 +28,11 @@ export default class App extends Component {
     );
   }
   render() {
-    const {isLoaded, error} = this.state;
+    const {isLoaded, error, weather} = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true}/>
-        {isLoaded ? <Weather/> : 
+        {isLoaded ? <Weather weatherType={weather.type} temperature={Math.round(weather.temp-273.15)}/> : 
           <View style={styles.loading}>
             <Text style={styles.loadingText}>getting the weather data</Text>
             {error? <Text style={styles.errorText}>{error}</Text>:null}
@@ -36,7 +40,22 @@ export default class App extends Component {
       </View>
     );
   }
-  
+
+  _fetchWeather =(latitude, longitude) =>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?` //apostrophe 와 single quarter의 차이가 뭘까
+          +`lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+    .then(response => response.json())
+    .then(json=>{
+      this.setState({
+        isLoaded: true,
+        weather:{
+          temp: json.main.temp,
+          type: json.weather["0"].main,
+        }
+      })
+      console.log(json)
+    });
+  }
 }
 
 const styles = StyleSheet.create({
--- Weather.js
+++ Weather.js
@@ -2,24 +2,78 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {LinearGradient} from 'expo';
 import {Ionicons} from '@expo/vector-icons';
+import PropTypes from 'prop-types';
 
-export default class Weather extends Component{
-    render(){
-        return <LinearGradient 
-        colors={['#132213','#212faa','#005bea']} 
+// export default class Weather extends Component{
+//     render(){
+//         return <LinearGradient 
+//         colors={['#132213','#212faa','#005bea']} 
+//         style={styles.container}>
+
+//             <View style={styles.upper}>
+//                 <Ionicons color='#fff' size={144} name='ios-rainy'/>
+//                 <Text style={styles.temp}>35˚</Text>
+//             </View>
+//             <View style={styles.lower}>
+//                 <Text style={styles.mainMsg}>main msg</Text>
+//                 <Text style={styles.subMsg}>sub msg</Text>
+//             </View>
+
+//         </LinearGradient>
+//     }
+// }
+const weatherCases = {
+    Rain: {
+        colors:['#00c6fb','#05bea'],
+        mainMsg: '비',
+        subMsg: '비옴',
+        icon: 'ios-rainy'
+    },
+    Clear: {
+        colors:['#fef253','#ff7300'],
+        mainMsg: '맑음',
+        subMsg: '맑은날',
+        icon: 'ios-sunny'
+    },
+    Thunderstorm: {
+        colors:['#00ecbc','#007adf'],
+        mainMsg: '천둥번개',
+        subMsg: '낙뢰주의',
+        icon: 'ios-thunderstorm'
+    },
+    Clouds: {
+        colors:['#d7d2cc','#304352'],
+        mainMsg: '구름',
+        subMsg: '흐린날',
+        icon: 'ios-cloudy'
+    },
+}
+
+function Weather({temperature, weatherType}){
+    console.log(temperature)
+    console.log(weatherType)
+    return (
+        <LinearGradient 
+        colors={weatherCases[weatherType].colors}
         style={styles.container}>
 
             <View style={styles.upper}>
-                <Ionicons color='#fff' size={144} name='ios-rainy'/>
-                <Text style={styles.temp}>35˚</Text>
+                <Ionicons color='#fff' size={144} name={weatherCases[weatherType].icon}/>
+                <Text style={styles.temp}>{temperature}˚</Text>
             </View>
             <View style={styles.lower}>
-                <Text style={styles.mainMsg}>main msg</Text>
-                <Text style={styles.subMsg}>sub msg</Text>
+                <Text style={styles.mainMsg}>{weatherCases[weatherType].mainMsg}</Text>
+                <Text style={styles.subMsg}>{weatherCases[weatherType].subMsg}</Text>
             </View>
 
         </LinearGradient>
-    }
+    )
+}
+export default Weather;
+
+Weather.proptypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherType: PropTypes.string.isRequired,
 }
 
 const styles = StyleSheet.create({
